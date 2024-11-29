f65dbf63d83812683da73f38f7b92ff9cbac5a95
Christian Calderon <chcalderon@gmail.com>
2024-11-29 19:54:13 -0300

    Crud Category 2nd part

--- Ecommerce/dashboard/src/store/Reducers/categoryReducer.js
+++ Ecommerce/dashboard/src/store/Reducers/categoryReducer.js
@@ -115,6 +115,13 @@ export const categoryReducer = createSlice({
                 state.loader = false;
                 state.errorMessage = payload.error;
             })
+            .addCase(deleteCategory.fulfilled, (state, action) => {
+                state.categorys = state.categorys.filter(cat => cat._id !== action.meta.arg);
+                state.successMessage = action.payload.message; 
+            })
+            .addCase(deleteCategory.rejected, (state,action) => { 
+                state.errorMessage = action.payload; 
+            })
 
     }
 
